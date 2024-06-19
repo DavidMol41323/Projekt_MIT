9d4093fe04ff2505e52fe7bc1f44c5da8affc7fd
David <mol41323@spseol.cz>
2024-06-19 18:39:34 +0200

    test- zapinani ledky tlacitkem

--- src/main.c
+++ src/main.c
@@ -5,41 +5,62 @@
 #include "milis.h"
 //#include "delay.h"
 //#include "uart1.h"
+#include "swi2c.h"
+
+//#include <stm/stm8s_i2c.h>
+
+#define GREEN_LED_0_PORT GPIOD
+#define GREEN_LED_0_PIN GPIO_PIN_0
+
+#define RED_LED_1_PORT GPIOD
+#define RED_LED_1_PIN GPIO_PIN_6
+/* #define RED_LED_2_PORT GPIOD
+#define RED_LED_2_PIN GPIO_PIN_2
+#define RED_LED_3_PORT GPIOD
+#define RED_LED_3_PIN GPIO_PIN_3
+#define RED_LED_4_PORT GPIOD
+#define RED_LED_4_PIN GPIO_PIN_4 */
+
+#define BTN1_PORT GPIOD
+#define BTN1_PIN GPIO_PIN_5
+
+
+
+
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
 
+    GPIO_Init(RED_LED_1_PORT, RED_LED_1_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    
+    GPIO_Init(BTN1_PORT, BTN1_PIN, GPIO_MODE_IN_PU_NO_IT);
+    
+    
+    
+    
     init_milis();
     //init_uart1();
 }
 
 
-int main(void)
-{
-  
-    uint32_t time = 0;
+int main(void)  {
 
     init();
 
-    while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
+    while(1) {
+    
+        if (GPIO_ReadInputPin(BTN1_PORT, BTN1_PIN) == RESET) {
+            GPIO_WriteHigh(RED_LED_1_PORT, RED_LED_1_PIN);
         }
-        //delay_ms(333);
+            
+        else {
+            GPIO_WriteLow(RED_LED_1_PORT, RED_LED_1_PIN);
+        }
     }
 }
+
 
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
